161a1ef5a9ea95716fcc6e7f0a8510b1c6fbded1
iLampard <162405siqiao>
2017-09-07 14:02:31 +0800

    fix path

--- xutils/__init__.py
+++ xutils/__init__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
-from .custom_logger import (LogFormatter,
-                            CustomLogger)
-from .string_utils import to_unicode
-from test_runner import (add_parent_path,
-                         TestRunner)
-from decorators import handle_exception
-from config_utils import (find_file,
-                          find_and_parse_config)
+from xutils.custom_logger import (LogFormatter,
+                                  CustomLogger)
+from xutils.string_utils import to_unicode
+from xutils.test_runner import (add_parent_path,
+                                TestRunner)
+from xutils.decorators import handle_exception
+from xutils.config_utils import (find_file,
+                                 find_and_parse_config)
 
 __all__ = ['LogFormatter',
            'CustomLogger',
@@ -19,4 +19,4 @@ __all__ = ['LogFormatter',
            'find_file',
            'find_and_parse_config']
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
