9ddd573215a67ba0a92669a7aca65fc140d01e02
qinchaoyi <qcy_1993@126.com>
2017-12-21 14:12:58 +0800

    新增判断：若无收件人和主题，则不发送邮件。

--- xutils/decorators.py
+++ xutils/decorators.py
@@ -7,7 +7,7 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def _send(*args, **kwargs):
+def _send(**kwargs):
 
     subject = kwargs.get('subject')
     email_dict = kwargs.get('email_dict')
@@ -48,21 +48,25 @@ def handle_exception(logger, **kw_decorator):
                 logger.info('Exception in function {0} -- {1}'.format(query_func.__name__, e))
 
                 subject = kw_decorator.get('subject')
-                if subject is not None:  # 发送email
-
-                    logger.info('Now is sending the email with exception message')
-                    t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-                    msg = t + ' ' + 'Exception in function {0} -- {1}'.format(query_func.__name__, e)
+                if subject is not None:  # 必须填写主题
                     email_dict = kw_decorator.get('email_dict')
                     if email_dict is not None:  # 用 email_dict 中的内容
                         pass
                     else:
                         email_dict = kw_decorator
 
-                    email_dict['text'] = msg
-                    _send(subject=subject, email_dict=email_dict)
+                    receiver = email_dict.get('receiver')
 
-                    logger.info('Email is sent')
+                    if receiver is not None: # 配置了邮件发送相关的信息（只判断了receiver是否填写）
+
+                        logger.info('Now is sending the email with exception message')
+                        t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+                        msg = t + ' ' + 'Exception in function {0} -- {1}'.format(query_func.__name__, e)
+
+                        email_dict['text'] = msg
+                        _send(subject=subject, email_dict=email_dict)
+
+                        logger.info('Email is sent')
 
         return wrapper
 
