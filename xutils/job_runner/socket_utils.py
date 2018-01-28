ad07d1f4df68bd9f5049d2c439cf5cb6d0f0fc2a
iLampard <siqiao_xue@163.com>
2018-01-28 22:28:51 +0800

    fix build

--- xutils/job_runner/socket_utils.py
+++ xutils/job_runner/socket_utils.py
@@ -1,28 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from ctypes import *
-from win32gui import *
-import pandas as pd
 
-user32 = windll.user32
-kernel32 = windll.kernel32
-psapi = windll.psapi
+try:
+    from win32gui import (windll,
+                          IsWindow,
+                          IsWindowVisible,
+                          GetWindowText,
+                          EnumWindows)
+
+    user32 = windll.user32
+    kernel32 = windll.kernel32
+    psapi = windll.psapi
+except ImportError:
+    pass
+import pandas as pd
 
 
 # 枚举窗口的回调函数
 def enum_windows_callback(hwnd, mouse):
     pid = c_ulong()
     # if IsWindow(hwnd) and IsWindowEnabled(hwnd) and IsWindowVisible(hwnd):
-    if IsWindow(hwnd) and IsWindowVisible(hwnd):
-        # if IsWindow(hwnd):
-        user32.GetWindowThreadProcessId(hwnd, byref(pid))
-        caption = GetWindowText(hwnd)
-        if len(caption) == 0:
-            return
-        global handles, pids, captions
-        handles.append(hwnd)
-        pids.append(pid.value)
-        captions.append(caption)
+    try:
+        if IsWindow(hwnd) and IsWindowVisible(hwnd):
+            # if IsWindow(hwnd):
+            user32.GetWindowThreadProcessId(hwnd, byref(pid))
+            caption = GetWindowText(hwnd)
+            if len(caption) == 0:
+                return
+            global handles, pids, captions
+            handles.append(hwnd)
+            pids.append(pid.value)
+            captions.append(caption)
+    except NameError:
+        pass
 
 
 # 供外部调用
@@ -31,6 +42,11 @@ def get_window_info():
     handles = []
     pids = []
     captions = []
-    EnumWindows(enum_windows_callback, 0)  # 枚举所有窗口
+    try:
+        EnumWindows(enum_windows_callback, 0)  # 枚举所有窗口
+    except NameError:
+        pass
     handle_df = pd.DataFrame(data={'handle': handles, 'pid': pids, 'caption': captions})
     return handle_df
+
+
